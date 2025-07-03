8337cc5dc93cd201e357e6274971117910bb3b65
Arthur Teruel <contato.arthurteruel@gmail.com>
2025-07-03 16:10:17 -0300

    Delete projeto_leilao_2025-main/projeto_leilao_2025-main directory

--- projeto_leilao_2025-main/projeto_leilao_2025-main/backend/src/main/java/com/leilao/backend/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.leilao.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.leilao.backend.model.Pessoa;
-
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
-
-}
